e7d2a6c8823886df17ffda9f3f4d3d5726c0b5c8
Vanspage <dovanwork@gmail.com>
2025-04-09 16:04:34 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const venom = require('venom-bot');
+
+venom
+  .create()
+  .then((client) => start(client))
+  .catch((error) => {
+    console.log(error);
+  });
+
+function start(client) {
+  client.onMessage((message) => {
+    const msg = message.body.toLowerCase();
+
+    if (message.isGroupMsg === false) {
+      if (msg === 'halo' || msg === 'hi') {
+        client.sendText(message.from, 'Hai! Selamat datang di layanan otomatis kami. Ketik *menu* untuk info.');
+      }
+
+      if (msg === 'menu') {
+        client.sendText(message.from, '📋 *Menu:*
+1. Cek saldo
+2. Tambah transaksi');
+      }
+    }
+  });
+}
